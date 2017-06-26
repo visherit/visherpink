85a34fd4d56b72a7f2b161fece2557210fd3db79
Visherit <visherit@gmail.com>
2017-06-26 03:30:38 +0300

    готовая сборка без лайврелоада

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt){
    grunt.loadNpmTasks("grunt-contrib-sass");
+    grunt.loadNpmTasks('grunt-contrib-watch');
     
     grunt.initConfig({
         pkg:grunt.file.readJSON('package.json'),
